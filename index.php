6205ac2d2392b807332eb0d70fe43949627f1dde
Chrysanths <112877815+Chrysanths@users.noreply.github.com>
2022-10-05 21:32:13 +0700

    php

---
+++ index.php
@@ -0,0 +1,93 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cổng thông tin tiêm chủng Covid-19</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <div class="header">
+        <div class="nav">
+            <a href="#">
+                <img src="image/CVM-Logo.png" alt="CVM-Logo">
+
+                <span class="title">CỔNG THÔNG TIN TIÊM CHỦNG COVID-19</span> 
+            </a>
+
+            <div class="nav-right">
+                <div>
+                    <ul>
+                        <li>
+                            <a href="#">Tin tức</a>
+                        </li>
+
+                        <li>
+                            <a href="#">Lịch tiêm</a>
+                        </li>
+
+                        <li>
+                            <a href="#">Tiêm chủng</a>
+                        </li>
+
+                        <li>
+                            <a href="#">Thống kê</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <a class="btnLogin" href="#">Đăng nhập</a>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="slider">
+
+        <div class="slideshow-container">
+
+            <div class="mySlides fade">
+                <img src="image/banner_with_flag.png" style="width:100%">
+            </div>
+            
+            <div class="mySlides fade">
+                <img src="image/banner_covid19.png" style="width:100%">
+            </div>
+            
+            <div class="mySlides fade">
+                <img src="image/banner_vaccine.png" style="width:100%">
+            </div>
+            
+            <div class="mySlides fade">
+                <img src="image/banner_codong.png" style="width:100%">
+            </div>
+            
+            </div>
+            
+            <div class="frame-dot" style="text-align:center">
+                <span class="dot"></span> 
+                <span class="dot"></span> 
+                <span class="dot"></span> 
+                <span class="dot"></span> 
+            </div>
+
+        <script type="text/javascript" src="main.js">
+        </script>
+    </div>
+
+    <div class="content">
+
+    </div>
+
+    <footer>
+        <div>
+            <p>Thông tin liên hệ: Đặng Nghiệp Cường</p>
+            <p>Số điện thoại: 0123456789</p>
+            <p>Email: dangnghiepcuong@gmail.com </p>
+            <img src="image/Logo BỘ.png" alt="Logo Bộ Y Tế">
+        </div>
+    </footer>
+
+</body>
+</html>
