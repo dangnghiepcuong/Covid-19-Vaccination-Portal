232e868795f5587c28d254b4d112eb3d0a96ea73
Chrysanths <112877815+Chrysanths@users.noreply.github.com>
2022-11-02 14:13:32 +0700

    split header, footer (html & css files)

---
+++ headerORG.php
@@ -0,0 +1,56 @@
+    <link rel="stylesheet" href="header.css">
+
+    <!-- HEADER -->
+    <div class="header">
+        <a class="title" href="HomePageORG.php">
+            <img src="image/CVM-Logo.png" alt="CVM-Logo">
+            <span class="title">CỔNG THÔNG TIN TIÊM CHỦNG COVID-19</span>
+        </a>
+
+        <div class="nav">
+            <div>
+                <ul>
+                    <li>
+                        <a class="menu-section" href="#">Tin tức</a>
+                    </li>
+
+                    <li>
+                        <a class="menu-section" href="#">Lịch tiêm</a>
+                    </li>
+
+                    <li>
+                        <a class="menu-section" href="#">Văn bản</a>
+                    </li>
+
+                    <li>
+                        <a class="menu-section" href="#">Thống kê</a>
+                    </li>
+
+                </ul>
+            </div>
+            <a class="avatar" href="#">
+                <img src="image/Avatar-ORG.png" alt="Logo đơn vị tiêm chủng">
+            </a>
+        </div>
+
+        <div class="drop-down-menu" id="drop-down-menu-profile">
+            <div class="holder">
+                <ul>
+                    <li>
+                        <a href="#">Thông tin tài khoản</a>
+                    </li>
+
+                    <li>
+                        <a href="#">Thông tin tổ chức</a>
+                    </li>
+
+                    <li>
+                        <a href="#">Đăng xuất</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <div class="header-virtual"></div>
+    <!-- END HEADER -->
