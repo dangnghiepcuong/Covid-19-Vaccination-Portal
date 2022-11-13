2815bb11504e1e424375243628ada2721cebd769
Đặng Nghiệp Cường <20520418@gm.uit.edu.vn>
2022-11-13 17:08:46 +0700

    rat nhieu

--- js/index.js
+++ js/index.js
@@ -24,9 +24,11 @@ $(document).ready(function(){
         $("#form-container-login").css('display','block');
     })
 
-    $("#gradient-bg-faded").click(function(){
+    // HANDLE GRADIENT BG CLICK
+    $("#gradient-bg-faded, .btn-confirm").click(function(){
         $("#form-container-reg-acc").css('display','none');
         $("#form-container-login").css('display','none');
+        $(".form-popup-confirm").css('display','none');
         $("#gradient-bg-faded").css('display','none');
     })
 
@@ -46,4 +48,6 @@ $(document).ready(function(){
         }
         $("#form-login").submit();
     })
+
+
 })
