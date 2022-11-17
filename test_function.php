e1607e73a436dd93cb86e688e8604a5ab2c0ef4a
Đặng Nghiệp Cường <20520418@gm.uit.edu.vn>
2022-11-17 14:50:00 +0700

    uncomment

--- test_function.php
+++ test_function.php
@@ -8,50 +8,36 @@ function HandleLogin()
     
     setcookie('username',$username);
     
-    // include("DatabaseConnection.php");
+    include("DatabaseConnection.php");
 
-    // $sql = "select * from ACCOUNT where username='" . $username . "'";
-    // $command = oci_parse($connection, $sql);
-    // oci_execute($command);
+    $sql = "select * from ACCOUNT where username='" . $username . "'";
+    $command = oci_parse($connection, $sql);
+    oci_execute($command);
 
-    // $row = oci_fetch_array($command, OCI_ASSOC + OCI_RETURN_NULLS);
-    if (/*$row == false*/ false) {
+    $row = oci_fetch_array($command, OCI_ASSOC + OCI_RETURN_NULLS);
+    if ($row == false) {
         // $_SESSION['message'] = 'Thông tin tài khoản sai!';
-        // header('Location: index.php');
-        // echo '<script>alert("Thông tin tài khoản sai!")</script>';
         echo -1;
     } else {
-        if (/*$password == $row['PASSWORD']*/true)
+        if ($password == $row['PASSWORD'])
             {
-                switch (/*$row['ROLE']*/ 2) {
+                switch ($row['ROLE']) {
                     case 0:
-                        // header('Location: index.php');
-                        // echo '<script>alert("Trang chu")</script>';
                         echo 0;
                         break;
                     case 1:
-                        // setcookie('username', $username);
-                        // header('Location: HomePageORG.php');
-                        // echo '<script>alert("Trang dvtc")</script>';
                         echo 1;
                         break;
                     case 2:
-                        // setcookie('username', $username);
-                        // header('Location: HomePageCitizen.php');
-                        // echo '<script>alert("Trang cong dan")</script>';
                         echo 2;
                         break;
                     default:
-                        // header('Location: index.php');
                         echo -1;
                         break;
                 }
     
             } else {
-            // $_SESSION['message'] = 'Thông tin tài khoản sai!';
-            // header('Location:index.php');
             echo -1;
-            // echo '<script>alert("Thông tin tài khoản sai!")</script>';
         }
     }
 }
