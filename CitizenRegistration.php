7c191a6e902fe775ab35b7fdd5e7563612413853
Đặng Nghiệp Cường <20520418@gm.uit.edu.vn>
2022-11-26 18:35:17 +0700

    turn off error warning

--- CitizenRegistration.php
+++ CitizenRegistration.php
@@ -1,12 +1,10 @@
 <!DOCTYPE html>
 <?php
 include("object_Account.php");
-include("object_Register.php");
-
+include("object_Citizen.php");
 session_start();
 $citizen = new Citizen();
 $citizen = $_SESSION['CitizenProfile'];
-$Cregistration = new Register();
 // echo '<script>alert("' . $Cregistration->get_sched()->newOrg() . '")</script>'; 
 
 if (!(isset($_SESSION['AccountInfo']) && $_SESSION['AccountInfo']->get_status() == 1) && isset($_SESSION['CitizenProfile']))
@@ -99,67 +97,6 @@ if (!(isset($_SESSION['AccountInfo']) && $_SESSION['AccountInfo']->get_status() 
                 <br>
                 <div class="holder">
                     <div class="list-registration" id="list-registration">
-                        <?php
-
-                        include("DatabaseConnection.php");
-                        $sql = "select Name, ProvinceName, DistrictName, TownName, Street, TO_CHAR(OnDate, 'YYYY-MM-DD') OnDate, Time, NO, VaccineID, Serial, Status, DoseType, Image from (
-                                    (select SchedID, Time, NO, Status, REG.DoseType, OrgID, OnDate, VaccineID, Serial, Image from (
-                                        (select ID, SchedID, NO, Time, Status, DoseType, Image from REGISTER where CitizenID = :citizenid) REG
-                                        inner join
-                                        (select ID, OrgID, OnDate, VaccineID, Serial from SCHEDULE) SCHED
-                                        on
-                                        REG.SchedID = SCHED.ID)
-                                    ) REG_SCHED
-                                    inner join
-                                    (select ID, Name, ProvinceName, DistrictName, TownName, Street from ORGANIZATION) ORG
-                                    on REG_SCHED.OrgID = ORG.ID
-                                )";
-                        $command = oci_parse($connection, $sql);
-                        oci_bind_by_name($command, ':citizenid', $citizen->get_id());
-                        oci_execute($command);
-
-                        while (($row = oci_fetch_array($command, OCI_ASSOC + OCI_RETURN_NULLS)) != false) {
-                            $Cregistration->get_sched()->get_org()->set_name($row['NAME']);
-                            $Cregistration->get_sched()->get_org()->set_provincename($row['PROVINCENAME']);
-                            $Cregistration->get_sched()->get_org()->set_districtname($row['DISTRICTNAME']);
-                            $Cregistration->get_sched()->get_org()->set_townname($row['TOWNNAME']);
-                            $Cregistration->get_sched()->get_org()->set_street($row['STREET']);
-                            $Cregistration->get_sched()->set_ondate($row['ONDATE']);
-                            $Cregistration->set_time($row['TIME']);
-                            $Cregistration->set_NO($row['NO']);
-                            $Cregistration->get_sched()->set_vaccine($row['VACCINEID']);
-                            $Cregistration->get_sched()->set_serial($row['SERIAL']);
-                            $Cregistration->set_status($row['STATUS']);
-                            $Cregistration->set_dosetype($row['DOSETYPE']);
-                            $Cregistration->set_image($row['IMAGE']);
-
-                            echo '
-                            <div class="registration">
-                                <p class="obj-org-name">' . $Cregistration->get_sched()->get_org()->get_name() . '</p>
-                                <div class="holder-obj-attr">
-                                        <div class="obj-attr">
-                                            <p class="attr-address">Đ/c: '
-                                . $Cregistration->get_sched()->get_org()->get_provincename() . ', '
-                                . $Cregistration->get_sched()->get_org()->get_districtname() . ', '
-                                . $Cregistration->get_sched()->get_org()->get_townname()
-                                . '</p>
-                                            <p class="attr-date-time-no">Lịch tiêm ngày: ' . $Cregistration->get_sched()->get_ondate()
-                                . '&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Buổi ' . $Cregistration->get_time()
-                                . '&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp STT: ' . $Cregistration->get_no() . '</p>
-                                            <p class="attr-vaccine-serial">Vaccine: '
-                                . $Cregistration->get_sched()->get_vaccine() . ' - ' . $Cregistration->get_sched()->get_serial()
-                                . '&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Tình trạng: ' . $Cregistration->get_status() . '</p>
-                                        </div>
-
-                                        <div class="interactive-area">
-                                            <button class="btn-medium-bordered btn-cancel">Hủy</button>
-                                        </div>
-                                    </div>
-                            </div>
-                            ';
-                        }
-                        ?>
-
                     </div>
 
                 </div>
--- js/CitizenRegistration.js
+++ js/CitizenRegistration.js
@@ -24,35 +24,19 @@ $(document).ready(function () {
 
     // END LOAD FRONT END DATA
 
-    $(".list-registration").on('click', '.btn-cancel', function () {
-        date_time_no = $(this).parent().parent().find(".obj-attr").find(".attr-date-time-no").text();
-        vaccine = $(this).parent().parent().find(".obj-attr").find(".attr-vaccine-serial").text();
-        message = "Xác nhận hủy đăng ký tiêm chủng?<br><br>" + date_time_no + ", " + vaccine;
-        $("#form-popup-confirm .form-message").html(message);
+    LoadRegistration()
+    function LoadRegistration(){
+        $.ajax({
+            cache: false,
+            url: 'HandleLoadRegistration.php',
+            type: 'POST',
+            data: { method: 'LoadRegistration', status: status, vaccine: vaccine, time: time },
+            success: function(result) {
+                $('#list-registration').html(result);
+            },
+            error: function(error) {
 
-        $(".gradient-bg-faded").css('display', 'block');
-        $("#form-popup-confirm").css('display', 'block');
-    })
-    $("#form-popup-confirm").on('click', '.btn-cancel', function () {
-        $("#form-popup-confirm").css('display', 'none');
-        $(".gradient-bg-faded").css('display', 'none');
-    })
-
-    // DROP DOWN MENU
-    $(".header").on('mouseover', '.avatar', function () {
-        $("#drop-down-menu-profile").css('display', 'block');
-    })
-
-    $(".header").on('mouseleave', '.avatar', function () {
-        $("#drop-down-menu-profile").css('display', 'none');
-    })
-
-    $(".header").on('mouseleave', '#drop-down-menu-profile', function () {
-        $("#drop-down-menu-profile").css('display', 'none');
-    });
-
-    $(".header").on('mouseover', '#drop-down-menu-profile', function () {
-        $("#drop-down-menu-profile").css('display', 'block');
-    });
-
+            }
+        })
+    }
 })
