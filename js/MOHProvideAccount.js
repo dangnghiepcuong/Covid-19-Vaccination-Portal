8397b4091a6ebebf1a954363e9e3d491081b780b
Chrysanths <112877815+Chrysanths@users.noreply.github.com>
2022-11-27 16:33:13 +0700

    MOHProfile

---
+++ js/MOHProvideAccount.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+
+    // LOAD FRONT END DATA
+    menu_title = "<a href='MOHProfile.php'>Cấp tài khoản đơn vị</a>";
+    $("#function-navigation-bar-title").html(menu_title);
+
+    homepage = "<a href='index.php'>Trang chủ</a>";
+    $("#homepage-path").html(homepage);
+
+    subpage = "<a href='MOHProfile.php'>Đơn vị</a>"
+    $("#subpage-path").html(subpage);
+
+    selected_function = "<a href='MOHProfile.php'>Cấp tài khoản đơn vị</a>";
+    $("#selected-function-path").html(selected_function);
+
+    $("#function-menu-title").text("Đơn vị tiêm chủng");
+
+    menu = "<br><a href='MOHProvideAccount.php'><li>Cấp tài khoản đơn vị</li></a>";
+    menu += "<br><a href='#'><li>Quản lý đơn vị</li></a>";
+
+    $("#function-menu-list").find("ul").html(menu);
+    // END LOAD FRONT END DATA
+})
+
