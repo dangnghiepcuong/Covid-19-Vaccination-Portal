a8f489dc13592039613b6529b83a764729a67a18
lychthac <ccdl2002@gmail.com>
2022-12-01 15:52:03 +0700

    medical form list

--- MedicalFormSubmit.php
+++ MedicalFormSubmit.php
@@ -31,7 +31,7 @@ else {
     <script src="js/MedicalFormSubmit.js"></script>
     <script src="js/index.js"></script>
     <script src="js/WebElements.js"></script>
-    <title>Khai báo y tế</title>
+    <title>Tờ khai y tế</title>
 </head>
 
 <body>
