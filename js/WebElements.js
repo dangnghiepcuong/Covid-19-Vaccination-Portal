af4583eccd721da63485ae5c088a7beba0e5bf12
Chrysanths <112877815+Chrysanths@users.noreply.github.com>
2022-12-04 10:58:51 +0700

    forgot password: chua lam xong, hong co biet lam cho lam

--- js/WebElements.js
+++ js/WebElements.js
@@ -55,13 +55,14 @@ $(document).ready(function () {
     })
 
     // HANDLE GRADIENT BG CLICK
-    // $('#gradient-bg-faded, .btn-confirm').click(function () {
-    //     $('#form-container-reg-acc').css('display', 'none');
-    //     $('#form-container-login').css('display', 'none');
-    //     $('#form-popup-confirm').css('display', 'none');
-    //     $('#gradient-bg-faded').css('display', 'none');
-    //     $('#container-reg-profile').css('display', 'none');
-    //     $('.message').text('');
-    //     $('.form-popup').css('display', 'none');
-    // })
+    $('#gradient-bg-faded, .btn-confirm').click(function () {
+        $('#form-container-reg-acc').css('display', 'none');
+        $('#form-container-login').css('display', 'none');
+        $('#form-popup-confirm').css('display', 'none');
+        $('#gradient-bg-faded').css('display', 'none');
+        $('#container-reg-profile').css('display', 'none');
+        $('.message').text('');
+        $('.form-popup').css('display', 'none');
+        $('#container-forgot-password').css('display', 'none');
+    })
 })
